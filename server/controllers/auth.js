27d3b74f685fce8df3d7f49579ee875593cca353
Nileshrmore13 <Nileshrmore13@gmail.com>
2023-02-25 00:01:30 +0530

    Backend Config with authentication and authorization with middlewares

---
+++ server/controllers/auth.js
@@ -0,0 +1,67 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// Register new user
+
+export const register = async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json({ message: "Register Successfull", savedUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({ message: "User Does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    }
+
+    const token = jwt.sign({id: user._id},process.env.JWT_SECRET)
+    delete user.password;
+
+    res.status(201).json({token , user});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose, { mongo } from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+//  CONFIGURATION
+
+const __filename = fileURLToPath(import.meta.url); // to import file url when we are using type module to import
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// FILE STORAGE
+
+const storage = multer.diskStorage({
+  // whenever user uplaod file it with save to this destination
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+//  ROUTES WITH FILES
+
+app.post("/auth/register", upload.single("picture"),register)
+
+// Routes
+
+app.use("/auth",authRoutes)
+
+// MONGOOSE SETUP
+
+const port = process.env.PORT || 7000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`server is running on ${port}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
